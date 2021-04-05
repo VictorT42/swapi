de67ce81a0b15067851477f2b391225f3373f47a
Victor <tsak.victor@gmail.com>
2021-04-05 14:52:44 +0300

    Implement Level 1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import requests
+from sys import argv
 
-r = requests.get('https://www.swapi.tech/api/people/1')
+if argv[1] == 'search':
+    raw_result = requests.get(f'https://www.swapi.tech/api/people/?name={argv[2]}')
+    result = raw_result.json()['result']
+    if len(result) == 0:
+        print('\nThe force is not strong within you')
+        exit(0)
+    else:
+        character = result[0]['properties']
+        output = '\n' + \
+            f'Name: {character["name"]}\n' + \
+            f'Height: {character["height"]}\n' + \
+            f'Mass: {character["mass"]}\n' + \
+            f'Birth Year: {character["birth_year"]}'
+        print(output)
